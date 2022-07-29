420fe27f27facb6bc33d4352fbb4a1cfd82448de
morfiy <keks46@yandex.ru>
2022-07-29 15:17:17 +0300

    first

--- js/main.js
+++ js/main.js
@@ -11,3 +11,12 @@ function scrollPageUp() {
         behavior: 'smooth'
     })
 }
+document.addEventListener('DOMContentLoaded', () => {
+    // инициализация слайдера
+    new ItcSimpleSlider('.itcss', {
+        loop: true,
+        autoplay: true,
+        interval: 5000,
+        swipe: true,
+    });
+});
